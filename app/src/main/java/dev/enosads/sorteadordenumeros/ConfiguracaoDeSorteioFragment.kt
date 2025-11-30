f695bc946f9566a4bcadd29cc93453cdbd9f23d9
Enos Andrade <enosads@gmail.com>
2025-11-30 15:44:00 -0300

    Integrate navigation components and implement fragments for number generation setup and results. Update layouts, strings, and resources accordingly.

---
+++ app/src/main/java/dev/enosads/sorteadordenumeros/ConfiguracaoDeSorteioFragment.kt
@@ -0,0 +1,18 @@
+package dev.enosads.sorteadordenumeros
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class ConfiguracaoDeSorteioFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_configuracao_de_sorteio, container, false)
+    }
+}
